8b115c844b45702dba3eed175d4509fc05c0a7e2
Shubham Wagh <shubhamwagh48@gmail.com>
2023-03-29 15:15:16 +0100

    Update __init__.py

--- multiarmed_bandit/__init__.py
+++ multiarmed_bandit/__init__.py
@@ -1,1 +1,4 @@
-
+from .agent import Agent
+from .bandit import GaussianBandit, BinomialBandit, BernoulliBandit
+from .environment import Environment
+from .policy import EpsilonGreedyPolicy, GreedyPolicy, RandomPolicy
